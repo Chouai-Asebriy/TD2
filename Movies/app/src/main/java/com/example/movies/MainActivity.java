7cea9f183ea49f6d40bbf44ee3a981d2de1e7eb3
Chouai-Asebriy <developpement.mobileS9@gmail.com>
2021-12-07 16:38:36 +0100

    tp

---
+++ Movies/app/src/main/java/com/example/movies/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.movies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import android.view.MenuItem;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        //I added this if statement to keep the selected fragment when rotating the device
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new PopularFragment()).commit();
+        }
+    }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    switch (item.getItemId()) {
+                        case R.id.nav_popular:
+                            selectedFragment = new PopularFragment();
+                            break;
+                        case R.id.nav_upcomming:
+                            selectedFragment = new UpcommingFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+
+                    return true;
+                }
+            };
+}
---
+++ Movies/app/src/main/java/com/example/movies/MainFragment.java
@@ -0,0 +1,10 @@
+package com.example.movies;
+
+public class MainFragment {
+
+
+    /*public void updateDesignWhenUserClickedBottomView(String request){
+        this.refreshProjects(request);
+    }*/
+
+}
